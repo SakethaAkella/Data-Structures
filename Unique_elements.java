5dc2a96c3fffaa77def68cc2d23f9712e9cdd334
SakethaAkella <86010054+SakethaAkella@users.noreply.github.com>
2021-11-25 22:32:44 +0530

    Add files via upload

---
+++ Unique_elements.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    int n,i,j;
+	    int flag=0;
+	    Scanner input = new Scanner(System.in);
+	    System.out.print("Enter the number of elements in the array : ");
+	    n = input.nextInt();
+		int[] arr = new int[15];
+		System.out.print("Enter the elements in the array : ");
+		for(i=0;i<n;i++){
+		    arr[i] = input.nextInt();
+		}
+		int count=n;
+		for(i=0;i<n-1;i++){
+		    for(j=i+1;j<n;j++){
+		        if(arr[i]==arr[j]){
+		            count -= 1;
+		        }
+		    }
+		}
+		if(count!=0){
+		    System.out.println("Number of unique elements are "+count);
+		}
+	}
+}
