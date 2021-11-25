e16e28256de14c9d6cece9b8a11e465ca15cb3db
SakethaAkella <86010054+SakethaAkella@users.noreply.github.com>
2021-11-25 23:24:34 +0530

    Add files via upload

---
+++ Just_large.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    int n,i,j,temp,key,index=0;
+	    int flag=0;
+	    Scanner input = new Scanner(System.in);
+	    System.out.print("Enter the number of elements in the array : ");
+	    n = input.nextInt();
+		int[] arr = new int[15];
+		System.out.print("Enter the elements in the array : ");
+		for(i=0;i<n;i++){
+		    arr[i] = input.nextInt();
+		}
+		//sort the array
+		for(i=0;i<n;i++){
+		    for(j=1;j<n-1;j++){
+		        if(arr[j]>arr[j-1]){
+		            temp = arr[j];
+		            arr[j] = arr[j+1];
+		            arr[j+1] = temp;
+		        }
+		    }
+		}
+		System.out.print("Enter key : ");
+		key = input.nextInt();
+		for(i=0;i<n;i++){
+		    if(key<arr[i]){
+		        System.out.println("Just larger key is "+arr[i]);
+		        flag=1;
+		        break;
+		    }
+		}
+		if(flag==0){
+		    System.out.println("No larger key");
+		}
+	}
+}
